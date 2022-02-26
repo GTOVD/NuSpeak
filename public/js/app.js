fbf8d1ef8ce2fb0d18fb4b13aaafb29d0950becf
Thomas Vickers <vickerst10@gmail.com>
2022-02-25 20:02:28 -0700

    updates to input

--- public/js/app.js
+++ public/js/app.js
@@ -5602,15 +5602,54 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var _css_app_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./../../css/app.css */ "./resources/css/app.css");
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 
 
 
 function Post() {
+  var _React$useState = react__WEBPACK_IMPORTED_MODULE_0__.useState(""),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      count = _React$useState2[0],
+      setCount = _React$useState2[1];
+
+  var onStorageUpdate = function onStorageUpdate(event) {
+    var key = event.key,
+        newValue = event.newValue;
+
+    if (key === "count") {
+      setCount(newValue);
+    }
+  };
+
+  var handleChange = function handleChange(event) {
+    setCount(event.target.value);
+    localStorage.setItem("count", event.target.value);
+  };
+
+  react__WEBPACK_IMPORTED_MODULE_0__.useEffect(function () {
+    setCount(localStorage.getItem("count") || "");
+    window.addEventListener("storage", onStorageUpdate);
+    return function () {
+      window.removeEventListener("storage", onStorageUpdate);
+    };
+  }, []);
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.Fragment, {
     children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("input", {
-      value: "",
+      value: count,
+      onChange: handleChange,
       placeholder: "Create Post",
       className: "createpost"
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("textarea", {
--- resources/js/components/Post.js
+++ resources/js/components/Post.js
@@ -2,9 +2,36 @@ import React from "react";
 import "./../../css/app.css";
 
 export default function Post() {
+    const [count, setCount] = React.useState("");
+
+    const onStorageUpdate = (event) => {
+        const { key, newValue } = event;
+        if (key === "count") {
+            setCount(newValue);
+        }
+    };
+
+    const handleChange = (event) => {
+        setCount(event.target.value);
+        localStorage.setItem("count", event.target.value);
+    };
+
+    React.useEffect(() => {
+        setCount(localStorage.getItem("count") || "");
+        window.addEventListener("storage", onStorageUpdate);
+        return () => {
+            window.removeEventListener("storage", onStorageUpdate);
+        };
+    }, []);
+
     return (
         <>
-            <input value="" placeholder="Create Post" className="createpost" />
+            <input
+                value={count}
+                onChange={handleChange}
+                placeholder="Create Post"
+                className="createpost"
+            />
             <textarea className="textarea" placeholder="Text (optional)" />
         </>
     );
